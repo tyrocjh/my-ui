54b462e6a29fd38b79a12829d0ff09e0d0b01814
chenjiahui <chenjiahui@dossen.com>
2022-11-22 15:25:35 +0800

    修复window is not defined

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,8 +1,6 @@
-import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-import DPagination from '../../packages/pagination'
 import DDialog from '../../packages/dialog'
 import DSelect from '../../packages/select'
 import Table from '../../packages/table'
@@ -12,16 +10,29 @@ import DProgress from '../../packages/progress'
 import DSteps from '../../packages/steps'
 import DGlobalLoading from '../../packages/global-loading'
 
-Vue.use(ElementUI, {
-  size: 'small',
-})
+export default async ({
+  Vue,
+  options, // 附加到根实例的一些选项
+  router, // 当前应用的路由实例
+  siteData, // 站点元数据
+  isServer // 当前应用配置是处于 服务端渲染 或 客户端
+}) => {
+  if(!isServer) {
+    Vue.use(ElementUI, {
+      size: 'small',
+    })
 
-Vue.use(DPagination)
-  .use(DDialog)
-  .use(DSelect)
-  .use(Table)
-  .use(DInputPassword)
-  .use(DBacktop)
-  .use(DProgress)
-  .use(DSteps)
-  .use(DGlobalLoading)
+    await import('../../packages/pagination').then(module => {
+      Vue.use(module.default)
+    })
+
+    Vue.use(DDialog)
+      .use(DSelect)
+      .use(Table)
+      .use(DInputPassword)
+      .use(DBacktop)
+      .use(DProgress)
+      .use(DSteps)
+      .use(DGlobalLoading)
+  }
+}
